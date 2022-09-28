7297ee159b686f2cc2ef03e20db190a2c389ea89
bendn <bend.n@outlook.com>
2022-09-29 06:34:37 +0700

    chore: optimize export.sh

--- export.sh
+++ export.sh
@@ -6,7 +6,8 @@ set -E
 set -o pipefail
 
 size_multiplier="$1" # yep, its a string.
-echo "<-----|${size_multiplier}|----->"
+
+root_path="$(pwd)"
 
 # round $1
 function round() {
@@ -22,7 +23,7 @@ function outline() {
     if [[ -n $pixels_to_outline ]]; then
         convert "$file" -bordercolor none -background '#404049' -alpha background -channel A -blur "$pixels_to_outline" -level 0,0% "$file"
     else
-        python3 outline.py "$file" "$size_multiplier" "$file"
+        python3 "$root_path"/outline.py "$file" "$size_multiplier" "$file"
     fi
 }
 
@@ -30,10 +31,7 @@ function outline() {
 # $2: in
 # $3: out
 function render() {
-    local size="$1"
-    local in="$2"
-    local out="$3"
-    resvg --shape-rendering crispEdges -w "$size" -h "$size" "$in" "$out"
+    resvg --shape-rendering crispEdges -w "$1" -h "$1" "$2" "$3"
 }
 
 # python **
@@ -41,31 +39,34 @@ function multiply_rounded() {
     round "$(bc -l <<<"$1*$2")"
 }
 
+# $1: output_path
+function multiplied_size() {
+    multiply_rounded "$(identify -format '%w' "$1")" "$size_multiplier"
+}
+
 echo -e "[4] icon.svg ➔ icon.png"
 render 128 icon.svg icon.png &
 
+cd _src_
 shopt -s globstar
-for i in _src_/**/*.svg; do
+for i in **/*.svg; do
     while [ "$(jobs -r | wc -l)" -ge 10 ]; do sleep .1; done
     [[ $i == *"template"* ]] && continue
-    name="${i%.svg}.png"
-    name="${name//_src_/}"
-    path="_original_$name" # _original_/file
-    if [[ -f $path ]]; then
-        size=$(multiply_rounded "$(identify -format '%w' "$path")" "$size_multiplier")
-        base=$(basename "$name")
-        mkdir -p sprites-override"${name//$base/}"
-        out="sprites-override$name"
+    r_path="${i%.svg}.png" # the folder relative to sprites_override, or _original_: units/gamma.png
+    original_path="$root_path/_original_/$r_path"
+    if [[ -f $original_path ]]; then
+        output_path="$root_path/sprites-override/$r_path"
+        [[ ! -d "${output_path%/*}" ]] && mkdir -p "${output_path%/*}" # dirname
         #shellcheck disable=SC2143
         if [[ 
-            ($name == *"turrets"* && $name != *"heat"* && $name != *"bases"* && $name != *"top"* && $name != *"liquid"*) ||
-            (-n $(grep -F "$name" "manual_outline")) ]]; then
-            (render "$size" "$i" "$out" && outline "$out") &
+            ($r_path == *"turrets"* && $r_path != *"heat"* && $r_path != *"bases"* && $r_path != *"top"* && $r_path != *"liquid"*) ||
+            (-n $(grep -F "$r_path" "$root_path/manual_outline")) ]]; then
+            (render "$(multiplied_size "$original_path")" "$i" "$output_path" && outline "$output_path") &
         else
-            render "$size" "$i" "$out" &
+            render "$(multiplied_size "$original_path")" "$i" "$output_path" &
         fi
 
-        echo -e "[${size_multiplier}] $i ➔ $out"
+        echo -e "[${size_multiplier}] $r_path"
     else
         echo -e "::warning file=${i}::No corresponding sprite" >&2
     fi
