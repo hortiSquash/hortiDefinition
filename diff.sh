8cf4b3a8a056c1a31c8e270a1c2513fb62f6f823
bendn <bend.n@outlook.com>
2022-09-29 08:50:53 +0700

    feat: print average similarity

--- diff.sh
+++ diff.sh
@@ -14,6 +14,16 @@ echo ::endgroup::
 echo ::group::diff
 
 cd sprites-override
+
+cat >simmilarity.py <<EOF
+#!/usr/bin/env python3
+
+def avg(ls):
+  return sum(ls) / len(ls)
+
+ls = [
+EOF
+chmod +x simmilarity.py
 shopt -s globstar
 for i in **/*.png; do
     if grep -q -F "$i" "$root_path/diff_overrides"; then
@@ -31,9 +41,15 @@ for i in **/*.png; do
             echo "[$i]: exactly the same"
         else
             echo "[$i]: $dissimilarity dissimilar"
+            echo -n "$dissimilarity," >>simmilarity.py
         fi
     fi
 done
+echo -e "]\nprint(round(abs(avg(ls * 100) - 100), 3))" >>simmilarity.py
+
+[[ -f "$GITHUB_STEP_SUMMARY" ]] &&
+    echo "# $(./simmilarity.py)% similar" >"$GITHUB_STEP_SUMMARY"
+echo "[average]: $(./simmilarity.py)% similar"
 
 [[ -n "$errors" ]] && exit 1 # for github actions to report a failure
 
